f0618e8f2c58573950c754f736b70df2c2aabb07
Paolo Rocca <hispanico94@gmail.com>
2018-09-15 10:33:44 +0200

    removed useless commented code

--- Guzzi Tracker/VCFactory.swift
+++ Guzzi Tracker/VCFactory.swift
@@ -4,33 +4,6 @@ class VCFactory {
     
     let motorcycleData: MotorcycleData
     
-//    let motorcycleList: [Motorcycle]?
-//    
-//    let filterProviders: [FilterProvider]
-//    
-//    let filterStorage: Ref<Array<FilterProvider>>
-//    let orderStorage: Ref<Array<Order>>
-    
-    
-//    init(motorcycleList: [Motorcycle]?) {
-//
-//        self.motorcycleList = motorcycleList
-//
-//        self.filterProviders = [MinMaxYear(),
-//                                Family(motorcycleList: self.motorcycleList),
-//                                Weight(motorcycleList: self.motorcycleList),
-//                                Displacement(motorcycleList: self.motorcycleList),
-//                                Bore(motorcycleList: self.motorcycleList),
-//                                Stroke(motorcycleList: self.motorcycleList),
-//                                Power(motorcycleList: self.motorcycleList),
-//                                Wheelbase(motorcycleList: self.motorcycleList),
-//                                StrokeCycle()]
-//
-//        self.filterStorage = Ref<Array<FilterProvider>>.init(self.filterProviders)
-//
-//        self.orderStorage = Ref<Array<Order>>.init([Order.init(id: .yearDescending, title: "Year descending", comparator: MotorcycleComparator.yearDescending)])
-//    }
-    
     init(motorcycleData: MotorcycleData) {
         self.motorcycleData = motorcycleData
     }
